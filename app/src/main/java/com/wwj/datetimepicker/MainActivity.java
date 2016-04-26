25333d82c61805c3f8d69476c5471de559bb216d
luteng <542170624@qq.com>
2016-04-26 15:35:38 +0800

    15 35

--- app/src/main/java/com/wwj/datetimepicker/MainActivity.java
+++ app/src/main/java/com/wwj/datetimepicker/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("","");
     }
 
     public void btnWriteExternalStorage(View view) {
